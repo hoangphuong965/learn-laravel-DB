2ea035bbee290ed7cf8fbc361e06f3cc55f0e49c
Phuong Hoang <you@example.com>
2022-12-04 22:09:53 +0700

    eloquent basic

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['rating', 'content', 'user_id'];
+
+    // protected static function booted()
+    // {
+    //     static::addGlobalScope('rating', function (Builder $builder) {
+    //         $builder->where('rating', '>', 2);
+    //     });
+    // }
+
+    // public function scopeRating($query, int $value = 4)
+    // {
+    //     return $query->where('rating', '>', $value);
+    // }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Comment;
+use App\Models\Reservation;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
@@ -17,156 +19,56 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    // $users = DB::table('users')->get();
-    // $users = DB::table('users')->pluck('email');
-    // $users = DB::table('users')->find(1);
+    // $results = User::select('name', 'email')
+    //     ->addSelect([
+    //         'worst_rating' => Comment::select('rating')
+    //             ->whereColumn('user_id', 'users.id')
+    //             ->orderBy('rating', 'asc')
+    //             ->limit(1)
+    //     ])
+    //     ->get()->toArray();
 
-    // $comments = DB::table('comments')->select('content as comment_content')->get();
-    // $comments = DB::table('comments')->select('user_id')->distinct()->get();
+    // $results = User::orderByDesc(
+    //     Reservation::select('check_in')
+    //         ->whereColumn('user_id', 'users.id')
+    //         ->orderBy('check_in', 'desc')
+    //         ->limit(1)
+    // )->select('id', 'name')->get()->toArray();
 
-    // $results = DB::table('comments')->count();
-    // $results = DB::table('comments')->sum('user_id');
-    // $results = DB::table('comments')->where('content', 'content')->doesntExist();
-
-    // $results = DB::table('rooms')->get();
-    // $results = DB::table('rooms')->where('price', '<', '200')->get();
-    // $results = DB::table('rooms')->where([
-    //     ['room_size', '2'],
-    //     ['price', '<', '400']
-    // ])->get();
-    // $results = DB::table('rooms')
-    //     ->where('room_size', '=', '2')
-    //     ->orWhere('price', '<', '400')
-    //     ->get();
-    // $results = DB::table('rooms')
-    //     ->where('price', '<', '400')
-    //     ->orWhere(function ($query) {
-    //         $query
-    //             ->where('room_size', '>', '1')
-    //             ->where('room_size', '<', '4');
-    //     })
-    //     ->get();
-
-    // $results = DB::table('rooms')->whereBetween('room_size', [1, 3])->get();
-    // $results = DB::table('rooms')->whereNotIn('id', [1, 2, 3])->get();
-
-    // $results = DB::table('users')
-    //     ->whereExists(function ($query) {
-    //         $query->select('id')
-    //             ->from('reservations')
-    //             ->whereRaw('reservations.user_id = users.id')
-    //             ->where('check_in', '=', '2022-12-02')
-    //             ->limit(1);
-    //     })
-    //     ->get();
-
-    // $results = DB::table('users')
-    //     // ->whereJsonContains('meta->skills', 'Laravel')
-    //     ->whereJsonContains('meta->settings->site_language', 'en')
-    //     ->get();
-
-    // $results = DB::table('users')
-    //     ->orderBy('name', 'desc')
-    //     ->get();
-
-    // $results = DB::table('users')
-    //     ->latest() // created_at default
-    //     ->first();
-
-    // $results = DB::table('users')
-    //     ->inRandomOrder() // created_at default
-    //     ->first();
-
-    // $results = DB::table('comments')
-    //     ->selectRaw('count(content) as number_of_5stars_comments, rating') // hoặc là count(id)
-    //     ->groupBy('rating')
-    //     ->where('rating', '=', 3) // hoặc là ->having('rating', '=', 3)
-    //     ->get();
-
-    // $results = DB::table('comments')
-    //     ->skip(5) // bỏ qua 5 cái đầu
-    //     ->take(5) // lấy 5 cái tiếp theo
-    //     ->get();
-
-    // $results = DB::table('comments')
-    //     ->offset(5) // bỏ qua 5 cái đầu
-    //     ->limit(5) // lấy 5 cái tiếp theo
-    //     ->get();
-
-    // $room_id = 2;
-    // $results = DB::table('reservations')
-    //     ->when($room_id, function ($query, $room_id) {
-    //         return $query->where('room_id', $room_id);
-    //     })
-    //     ->get();
-
-    // $sortBy = null;
-    // $results = DB::table('rooms')
-    //     ->when($sortBy, function ($query, $sortBy) {
-    //         return $query->orderBy($sortBy);
-    //     }, function ($query) {
-    //         return $query->orderBy('price');
-    //     })
-    //     ->get();
-
-    // $results = DB::table('comments')->orderBy('id')->chunk(2, function ($comments) {
-    //     foreach ($comments as $comment) {
-    //         if ($comment->id == 5) return false;
+    // $results = Reservation::chunk(2, function ($reservations) {
+    //     foreach ($reservations as $reservation) {
+    //         echo $reservation->id;
     //     }
     // });
 
-    // day la phuong phap update cai gi do
-    // $results = DB::table('comments')->orderBy('id')->chunkById(5, function ($comments) {
-    //     foreach ($comments as $comment) {
-    //         DB::table('comments')
-    //             ->where('id', $comment->id)
-    //             ->update(['rating' => null]);
-    //     }
-    // }); // useful for administration tasks
+    // $results = User::find(1)->toArray();
+    // $results = User::where('email', 'like', '%@%')->first()->toArray();
+    // $results = User::where('email', 'like', '%@example.com%')->firstOr(function () {
+    //     User::where('id', 1)->update(['email' => 'lyly92@example.com']);
+    // });
+    // $results = User::findOrFail(100); // firstOrFail also possible
+    // $results = Comment::withoutGlobalScope('rating')->get();
+    // $results = Comment::rating(1)->get();
+    // $results = Comment::all()->toArray();
+    // $results = Comment::all()->count();
+    // $results = Comment::all()->toJson();
 
-    // ========================================================================
-    // cách 1
-    // $results = DB::table('reservations')
-    //     ->join('rooms', 'reservations.room_id', '=', 'rooms.id')
-    //     ->join('users', 'reservations.user_id', '=', 'users.id')
-    //     ->where('rooms.id', '>', 3)
-    //     ->where('users.id', '>', 1)
-    //     ->get();
+    // $comments = Comment::all();
+    // $results = $comments->map(function ($comment) {
+    //     return $comment->content;
+    // });
+    // $results = $comments->reject(function ($comment) {
+    //     return $comment->rating < 3;
+    // });
+    // $results = $comments->diff($results);
 
-    // cách 2
-    // $results = DB::table('reservations')
-    //     ->join('rooms', function ($join) {
-    //         $join->on('reservations.room_id', '=', 'rooms.id')
-    //             ->where('rooms.id', '>', 3);
-    //     })
-    //     ->join('users', function ($join) {
-    //         $join->on('reservations.user_id', '=', 'users.id')
-    //             ->where('users.id', '>', 1);
-    //     })
-    //     ->get();
+    $results = Comment::create([
+        'user_id' => 1,
+        'rating' => 5,
+        'content' => 'comment content',
+    ]);
 
-    // cách 3
-    // $rooms = DB::table('rooms')->where('id', '>', 3);
-    // $users = DB::table('users')->where('id', '>', 1);
-    // $results = DB::table('reservations')
-    //     ->joinSub($rooms, 'rooms', function ($join) {
-    //         $join->on('reservations.room_id', '=', 'rooms.id');
-    //     })
-    //     ->joinSub($users, 'users', function ($join) {
-    //         $join->on('reservations.user_id', '=', 'users.id');
-    //     })
-    //     ->get();
-    // ============================================================================
-
-    $results = DB::table('rooms')
-        ->leftJoin('reservations', 'rooms.id', '=', 'reservations.room_id')
-        ->leftJoin('cities', 'reservations.city_id', '=', 'cities.id')
-        ->selectRaw('room_size, cities.name, count(reservations.id) as reservations_count')
-        ->groupBy('room_size', 'cities.name')
-        ->orderByRaw('count(reservations.id) DESC')
-        ->get();
-
-    dump($results);
+    dump($results->toArray());
 
     return view('welcome');
 });
